ff570917ae04108270854a4d1e31dd122bc88066
jmwingo <jmwingo@asu.edu>
2020-10-04 14:19:19 -0700

    Initial version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,63 @@
+package cse360assignment02;
+
+/**
+* AddingMachine is the main class of this assignment.
+* <p>
+* It is responsible for adding and subtracting numbers and printing out its history.
+* <p>
+* @author jmwingo
+*/
+public class AddingMachine
+{
+	  private int total;
+	  
+	  /**
+	  * Constructor for AddingMachine object
+	  * <p>
+	  * sets total to 0
+	  * <p>
+	  */
+	  public AddingMachine ()
+	  {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	  * getTotal() returns the value of the total variable
+	  * </p>
+	  * @return the calculated total
+	  */
+	  public int getTotal ()
+	  {
+	    return 0;
+	  }
+	  
+	  /**
+	  * add() adds the parameter to the total
+	  */
+	  public void add (int value)
+	  {
+	  }
+	  
+	  /**
+	  * subtract() subtracts the parameter from the total
+	  */
+	  public void subtract (int value)
+	  {
+	  }
+
+	  /**
+	  * toString() returns the history of transactions
+	  */
+	  public String toString ()
+	  {
+	    return "";
+	  }
+
+	  /**
+	  * clear() clears all transactions and memory
+	  */
+	  public void clear()
+	  {
+	  }
+}
